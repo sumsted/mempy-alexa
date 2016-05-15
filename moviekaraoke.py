e41ecd8e3b1d18adf7d7f6b43afbc272a237a4c5
Scott Umsted <scottumsted@gmail.com>
2016-05-15 08:51:27 -0500

    beautify

--- moviekaraoke.py
+++ moviekaraoke.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 
 
-
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
